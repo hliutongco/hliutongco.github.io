3ac128afa9eb1edc9a988c670a15146838d0205c
Helen Liutongco <liutongcoh@gmail.com>
2019-03-05 12:05:22 -0500

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-### Helen's Personal Website
-
-Yup, this is just my personal website/portfolio. Nothing to see here, folks.
---
+++ precache-manifest.8459cb071c52ec8e30bd6f8360504489.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0e93b7f2c716879d906a",
+    "url": "/static/js/main.0e93b7f2.chunk.js"
+  },
+  {
+    "revision": "45def2acc2bdb561d633",
+    "url": "/static/js/2.45def2ac.chunk.js"
+  },
+  {
+    "revision": "0e93b7f2c716879d906a",
+    "url": "/static/css/main.f9689d45.chunk.css"
+  },
+  {
+    "revision": "ed5332a862b60475176ad856554689b9",
+    "url": "/index.html"
+  }
+];
