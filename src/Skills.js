3ca828cf96b00d1df0e29122e5d87ddadbb5c9c7
Helen Liutongco <liutongcoh@gmail.com>
2019-03-05 18:23:36 -0500

    Make mobile responsive

--- src/Skills.js
+++ src/Skills.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Skills = (props) => {
   return (
     <div id="skills" className="header-padding">
-      <h1>Skills</h1>
+      <h1>Helen's Skills</h1>
 
 
       <div>
-        <img src="https://s3.us-east-2.amazonaws.com/misc-helen/confidence-levels.png" alt="helen's skills"/>
+        <img src="https://s3.us-east-2.amazonaws.com/misc-helen/expertise.png" alt="helen's skills"/>
         <ul>
-          <li>The chart to the right depicts Helen's skills and her approximate confidence levels in each.</li>
-          <li>The x-axis denotes how much or how little confidence she has in each topic.</li>
+          <li>This chart depicts Helen's skills and her level of mastery over each.</li>
+          <li>The x-axis denotes how much expertise she has in each topic.</li>
           <li>This page was most recently updated on March 2019.</li>
         </ul>
       </div>
