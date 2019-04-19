2fc2e996e26dceb9c710647447db8b3749c1f76c
Helen Liutongco <liutongcoh@gmail.com>
2019-04-19 16:13:46 -0400

    add status quote

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -6,7 +6,7 @@ const threeKings = (
     <div>
       <ul>
         <li><strong>Libraries</strong>: React, Redux</li>
-        <li><strong>Other Tools</strong>: CSS with Sass, React State Hook, AWS S3 (for file storage), a whole lot of lifecycle methods, my meager creative writing abilities</li>
+        <li><strong>Other Tools</strong>: CSS with Sass, React state hook, a whole lot of lifecycle methods, my meager creative writing abilities</li>
         <li><strong>Github Repo</strong>: <a rel="noopener noreferrer" target="_blank" href="https://github.com/hliutongco/three-kings">Click here</a></li>
         <li><strong>Live Demo</strong>: <a rel="noopener noreferrer" target="_blank" href="http://three-kings.herokuapp.com">Click here</a></li>
       </ul>
@@ -29,7 +29,24 @@ const keyboardKaraoke = (
       </ul>
       <p><strong>Keyboard Karaoke</strong> is a typing game where you type the lyrics to a song as the song plays.
       This app uses a Javascript frontend and a simple Node.js/Express/MongoDB backend for video and audio streaming, as well as saving scores.
-      Features include a high scores table and a selection of five songs of varying difficulties.</p>
+      Features include high scores for each song and a selection of five songs of varying difficulties.</p>
+    </div>
+  </div>
+)
+
+const statusQuote = (
+  <div className="portfolio-cards">
+    <img src="https://s3.us-east-2.amazonaws.com/misc-helen/threekings.gif" alt="Demo for Keyboard Karaoke"/>
+    <div>
+      <ul>
+        <li><strong>Libraries</strong>: React, Redux, Node.js, Express</li>
+        <li><strong>Other Tools</strong>: CSS, MongoDB, React state and useEffect hooks, React speech-to-text node package</li>
+        <li><strong>Github Repo</strong>: <a rel="noopener noreferrer" target="_blank" href="https://github.com/hliutongco/status-quote-v2-frontend">Click here</a></li>
+        <li><strong>Live Demo</strong>: <a rel="noopener noreferrer" target="_blank" href="https://status-quote-v2.firebaseapp.com/">Click here</a></li>
+      </ul>
+      <p><strong>Status Quote</strong> is a game where players guess famous movie lines. The app plays a series of movie clips, and when each clip pauses, the player says their guess out loud.
+      This app uses a simple Node.js/Express/MongoDB backend for video streaming, as well as saving scores.
+      Features include a high scores table, the ability to receive hints, and the ability to re-record guesses.</p>
     </div>
   </div>
 )
@@ -49,9 +66,9 @@ const Portfolio = (props) => {
       case "keyboardkaraoke":
         return keyboardKaraoke
       case "statusquote":
-        return ""
+        return statusQuote
       default:
-        return ""
+        return null
     }
   }
 
